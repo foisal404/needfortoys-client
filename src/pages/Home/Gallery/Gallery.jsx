78c60a10f9bf580570eb1794c9abbc5d868d1c38
foisal404 <foisalahmmed2@gmail.com>
2023-05-21 01:16:35 +0600

    gallery section added

---
+++ src/pages/Home/Gallery/Gallery.jsx
@@ -0,0 +1,49 @@
+const Gallery = () => {
+  return (
+    <section className="pt-6 pb-16">
+      <h2 className="text-center lg:text-5xl text-3xl py-3">
+        Fuel Your Imagination !
+      </h2>
+      <div className="w-[80%] mx-auto grid grid-cols-3 lg:grid-cols-6 ">
+        <div className="">
+          <img
+            src="https://images.unsplash.com/photo-1530328411047-7063dbd29029?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1171&q=80"
+            alt="Drink"
+          />
+        </div>
+        <div className="">
+          <img
+            src="https://images.unsplash.com/photo-1531048324985-45ab12882e58?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80"
+            alt="Drink"
+          />
+        </div>
+        <div className="">
+          <img
+            src="https://images.unsplash.com/photo-1534767125632-29d5e051f715?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80"
+            alt="Drink"
+          />
+        </div>
+        <div className="">
+          <img
+            src="https://images.unsplash.com/photo-1534530202060-e4e00c9258b1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80"
+            alt="Drink"
+          />
+        </div>
+        <div className="">
+          <img
+            src="https://images.unsplash.com/photo-1532330393533-443990a51d10?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80"
+            alt="Drink"
+          />
+        </div>
+        <div className="">
+          <img
+            src="https://images.unsplash.com/photo-1533574962572-33521ba10b6d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80"
+            alt="Drink"
+          />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Gallery;
