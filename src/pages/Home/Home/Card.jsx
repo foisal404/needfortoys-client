0123132521daa19366e6722fbfca29f730222ab1
foisal404 <foisalahmmed2@gmail.com>
2023-05-21 19:36:46 +0600

    Shop by category added using react tab

---
+++ src/pages/Home/Home/Card.jsx
@@ -0,0 +1,11 @@
+
+const Card = ({toy}) => {
+    
+    return (
+        <div>
+            <h2>{toy.name}</h2>
+        </div>
+    );
+};
+
+export default Card;
--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,14 +1,78 @@
-
-import Banner from '../Banner/Banner';
-import Gallery from '../Gallery/Gallery';
+import { useLoaderData } from "react-router-dom";
+import Banner from "../Banner/Banner";
+import Gallery from "../Gallery/Gallery";
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import "react-tabs/style/react-tabs.css";
+import { useEffect, useState } from "react";
+import Card from "./Card";
 
 const Home = () => {
-    return (
-        <div>
-            <Banner></Banner>
-            <Gallery></Gallery>
-        </div>
-    );
+  const loadcate = useLoaderData();
+  const [toys, setToys] = useState([]);
+  const [url, setUrl] = useState("");
+  useEffect(() => {
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => setToys(data));
+  }, [url]);
+  const catagoryload = (name) => {
+    setUrl(`http://localhost:5000/toys?category_name=${name}`);
+  };
+  const subCatagoryload = (name) => {
+    setUrl(`http://localhost:5000/toys?subcategory_name=${name}`);
+  };
+  return (
+    <div>
+      <Banner></Banner>
+      <Gallery></Gallery>
+      <div className="text-center">
+        <Tabs>
+          <TabList>
+            {loadcate.map((cat) => (
+              <Tab key={cat.key}>
+                <a onClick={() => catagoryload(cat.name)}>{cat.name}</a>
+              </Tab>
+            ))}
+          </TabList>
+          {loadcate.map((cat) => (
+            <TabPanel key={cat._id}>
+              <div className="tabs flex justify-center">
+                <button
+                  className="tab "
+                  onClick={() => subCatagoryload(`${cat.subcategories[0]}`)}
+                >
+                  {cat.subcategories[0]}
+                </button>
+                <button
+                  className="tab "
+                  onClick={() => subCatagoryload(`${cat.subcategories[1]}`)}
+                >
+                  {cat.subcategories[1]}
+                </button>
+                <button
+                  className="tab "
+                  onClick={() => subCatagoryload(`${cat.subcategories[2]}`)}
+                >
+                  {cat.subcategories[2]}
+                </button>
+                <button
+                  className="tab "
+                  onClick={() => subCatagoryload(`${cat.subcategories[3]}`)}
+                >
+                  {cat.subcategories[3]}
+                </button>
+              </div>
+            </TabPanel>
+          ))}
+          <div>
+            {toys.map((toy) => (
+              <Card key={toy._id} toy={toy}></Card>
+            ))}
+          </div>
+        </Tabs>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
