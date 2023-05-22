70753d73b39c3ac4780d017c924af47cf9ff5dfe
foisal404 <foisalahmmed2@gmail.com>
2023-05-23 03:30:53 +0600

    Implement a search system on all toys page  based on the Toy name

--- src/pages/AllToys/AllToys.jsx
+++ src/pages/AllToys/AllToys.jsx
@@ -1,18 +1,43 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
+import { FaSearch } from "react-icons/fa";
 import { Link, useLoaderData } from "react-router-dom";
 
 const AllToys = () => {
   const loader = useLoaderData();
   const [toys, setToys] = useState(loader.slice(0, 20));
+  const inputref = useRef(null)
+  function handleClick() {
+    const q=inputref.current.value;
+    fetch(`https://mango-toys-server.vercel.app/toys?name=${q}`)
+    .then(res=>res.json())
+    .then(data=>setToys(data))
+  }
   // console.log(loader);
   return (
     <div>
       <div className="overflow-x-auto w-full p-5 px-10">
+        <div className="flex justify-between">
+          <h2 className="text-3xl font-mono">All Toys</h2>
+          <div className="pb-4">
+            <input
+              type="text"
+              placeholder="Search"
+              className="input input-bordered"
+              ref={inputref}
+            />
+            <button className="btn btn-ghost ml-2" onClick={handleClick}><FaSearch/></button>
+          </div>
+          <div>
+            <span className="text-xs">sort by</span>
+            <button className="btn mx-2">low price</button>
+            <button className="btn mx-2">high price</button>
+          </div>
+        </div>
         <table className="table w-full">
           {/* head */}
           <thead>
-            <tr  className="relative z-0" >
-              <th className="z-0" >About</th>
+            <tr className="relative z-0">
+              <th className="z-0">About</th>
               <th>Sub-category</th>
               <th>Price</th>
               <th>Available Quantity</th>
@@ -21,32 +46,33 @@ const AllToys = () => {
           </thead>
           <tbody>
             {toys.map((toy) => (
-              
-                <tr key={toy._id}>
-                  <td>
-                    <span className="flex items-center space-x-3">
-                      
-                      <span className="hidden md:block">
-                        <span className="font-bold">{toy?.name }</span>
-                        <span >Seller :{toy?.seller_name && toy?.seller_name }</span>
-                        <span className="text-sm opacity-50"></span>
+              <tr key={toy._id}>
+                <td>
+                  <span className="flex items-center space-x-3">
+                    <span className="hidden md:block">
+                      <span className="font-bold">{toy?.name}</span>
+                      <span className="ml-5">
+                        Seller :{toy?.seller_name && toy?.seller_name}
                       </span>
+                      <span className="text-sm opacity-50"></span>
                     </span>
-                  </td>
-                  <td>
-                    {/* {toy.category_name}
+                  </span>
+                </td>
+                <td>
+                  {/* {toy.category_name}
                     <br /> */}
-                    <span className="badge badge-ghost badge-sm">
-                      {toy?.subcategory_name}
-                    </span>
-                  </td>
-                  <td>${toy.price}</td>
-                  <td>{toy?.available_quantity}</td>
-                  <th>
-                    <button className="btn  btn-ghost btn-xs"><Link to={`/toy/${toy?._id}`}>Details</Link></button>
-                  </th>
-                </tr>
-            
+                  <span className="badge badge-ghost badge-sm">
+                    {toy?.subcategory_name}
+                  </span>
+                </td>
+                <td>${toy.price}</td>
+                <td>{toy?.available_quantity}</td>
+                <th>
+                  <button className="btn  btn-ghost btn-xs">
+                    <Link to={`/toy/${toy?._id}`}>Details</Link>
+                  </button>
+                </th>
+              </tr>
             ))}
           </tbody>
         </table>
